697ac27ba00aadc08068eaebdec16f02907d96f5
미니 <mini@minichip.net>
2025-04-07 20:02:46 +0900

    Update app.c

--- main/app.c
+++ main/app.c
@@ -1069,13 +1069,14 @@ static int dtmf_stream(struct ast_channel *chan, const char *digits, int between
 				break;
 			}
 		} else if (strchr("0123456789*#abcdfABCDF", *ptr)) {
-			if (*ptr == 'f' || *ptr == 'F') {
-				/* ignore return values if not supported by channel */
-				ast_indicate(chan, AST_CONTROL_FLASH);
-			} else {
-				/* Character represents valid DTMF */
-				my_senddigit(chan, *ptr, duration);
-			}
+			my_senddigit(chan, *ptr, duration);
+			// if (*ptr == 'f' || *ptr == 'F') {
+			// 	/* ignore return values if not supported by channel */
+			// 	ast_indicate(chan, AST_CONTROL_FLASH);
+			// } else {
+			// 	/* Character represents valid DTMF */
+			// 	my_senddigit(chan, *ptr, duration);
+			// }
 			/* pause between digits */
 			res = my_sleep(chan, between);
 			if (res) {
